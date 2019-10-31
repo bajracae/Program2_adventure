662d9a7e93c7921887c1e5d05029a9e80ed71d3d
Aeijan Bajracharya <bajracae@os1.engr.oregonstate.edu>
2019-10-31 02:01:24 -0700

    reads the name and connections

--- bajracae.adventure.c
+++ bajracae.adventure.c
@@ -1,67 +1,192 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-// Create all connections in graph
-while (IsGraphFull() == false)
-{
-  AddRandomConnection();
+void findLatestFile(char * newestDirName);
+struct room * createStruct();
+void readRoomNames(char * newestDirName, struct room * rooms);
+void readStructID(struct room * rooms);
+void readRoomType(char * newestDirName, struct room * rooms);
+void fillStruct(char * newestDirName, struct room * rooms);
+void printStruct(struct room * rooms);
+
+struct room {
+	int id;
+	char * name;
+	char * roomType;
+	int numConnection;
+	// char * connections[7];
+	char connections[7][256];
+	
+};
+
+int main() {
+    
+    char newestDirName[256];
+    findLatestFile(newestDirName); // Name of the latest file
+    
+    // DIR * gameDir = opendir(newestDirName);
+    struct room * rooms_struct_array = createStruct();
+    // readRoomNames(newestDirName, rooms_struct_array);
+    // readStructID(rooms_struct_array);
+	// readRoomType(newestDirName, rooms_struct_array);
+	fillStruct(newestDirName, rooms_struct_array);
+    printStruct(rooms_struct_array);
+    return 0;
 }
 
-// Returns true if all rooms have 3 to 6 outbound connections, false otherwise
-bool IsGraphFull()  
-{
-  ...
+void findLatestFile(char * newestDirName) {
+    // -------------------------------------------------------------------------------------------
+    // CITE this
+    // -------------------------------------------------------------------------------------------
+    int newestDirTime = -1; // Modified timestamp of newest subdir examined
+    char targetDirPrefix[32] = "bajracae.rooms."; // Prefix we're looking for
+    //char newestDirName[256]; // Holds the name of the newest dir that contains prefix
+    memset(newestDirName, '\0', sizeof(newestDirName));
+
+    DIR* dirToCheck; // Holds the directory we're starting in
+    struct dirent *fileInDir; // Holds the current subdir of the starting dir
+    struct stat dirAttributes; // Holds information we've gained about subdir
+
+    dirToCheck = opendir("."); // Open up the directory this program was run in
+
+    if (dirToCheck > 0) // Make sure the current directory could be opened
+    {
+      while ((fileInDir = readdir(dirToCheck)) != NULL) // Check each entry in dir
+      {
+        if (strstr(fileInDir->d_name, targetDirPrefix) != NULL) // If entry has prefix
+        {
+          // printf("Found the prefex: %s\n", fileInDir->d_name);
+          stat(fileInDir->d_name, &dirAttributes); // Get attributes of the entry
+
+          if ((int)dirAttributes.st_mtime > newestDirTime) // If this time is bigger
+          {
+            newestDirTime = (int)dirAttributes.st_mtime;
+            memset(newestDirName, '\0', sizeof(newestDirName));
+            strcpy(newestDirName, fileInDir->d_name);
+            // printf("Newer subdir: %s, new time: %d\n", fileInDir->d_name, newestDirTime);
+          }
+        }
+      }
+    }
+
+    closedir(dirToCheck); // Close the directory we opened
 }
 
-// Adds a random, valid outbound connection from a Room to another Room
-void AddRandomConnection()  
-{
-  Room A;  // Maybe a struct, maybe global arrays of ints
-  Room B;
-
-  while(true)
-  {
-    A = GetRandomRoom();
-
-    if (CanAddConnectionFrom(A) == true)
-      break;
-  }
-
-  do
-  {
-    B = GetRandomRoom();
-  }
-  while(CanAddConnectionFrom(B) == false || IsSameRoom(A, B) == true || ConnectionAlreadyExists(A, B) == true);
-
-  ConnectRoom(A, B);  // TODO: Add this connection to the real variables, 
-  ConnectRoom(B, A);  //  because this A and B will be destroyed when this function terminates
+struct room * createStruct() {
+	struct room * rooms = (struct room *)malloc(sizeof(struct room) * 7);
+	int i,j ;
+	for(i = 0; i < 7; i++) {
+		rooms[i].numConnection = 0;
+		for(j = 0; j < 7; j++) {
+			// rooms[i].connections[j]= "";
+			memset(rooms[i].connections[j], 0, 256);
+		}
+	}
+	return rooms;
 }
 
-// Returns a random Room, does NOT validate if connection can be added
-Room GetRandomRoom()
-{
-  ...
+// void readRoomNames(char * newestDirName, struct room * rooms) {
+//     struct dirent * filename;
+// 	DIR * gameDir = opendir(newestDirName);
+//     int i = 0;
+//     while((filename = readdir(gameDir)) != NULL) {
+//         if((strcmp(".", filename->d_name) != 0) && (strcmp("..", filename->d_name) != 0)) {
+//             rooms[i].name = filename->d_name;
+//             i++;
+//         }
+//     }
+// 	closedir(gameDir);
+// }
+
+// void readStructID(struct room * rooms) {
+// 	int counter = 0;
+// 	int i;
+// 	for(i = 0; i < 7; i++) {
+// 		rooms[i].id = counter;
+// 		counter = counter + 1;
+// 	}
+// }
+
+// void readConnection(DIR * gameDir, struct room * rooms) {
+//     struct dirent * filename;
+//     int i = 0;
+//     while((filename = readdir(gameDir)) != NULL) {
+// 
+//     }
+// }
+
+// void readRoomType(char * newestDirName, struct room * rooms) {
+//     struct dirent * filename;
+// 	DIR * gameDir = opendir(newestDirName);
+// 	char buffer[256];
+// 	FILE * file;
+// 	int i = 0;
+// 	printf("a\n");
+// 	while((filename = readdir(gameDir)) != NULL) {
+// 		file = fopen(rooms[i].name, "r");
+// 		printf("b\n");
+// 		while(fscanf(file, "ROOM TYPE: %s\n", buffer)) {
+// 			printf("bugger: %s\n", buffer);
+// 			if((strcmp(buffer, "START_ROOM") == 0) || (strcmp(buffer, "MID_ROOM") == 0) || (strcmp(buffer, "END_ROOM") == 0)) {
+// 				rooms[i].roomType = buffer;
+// 			}
+// 		}
+// 		i++;
+// 	}
+// 	closedir(gameDir);
+// }
+
+void fillStruct(char * newestDirName, struct room * rooms) {
+	struct dirent * filename;
+	DIR * gameDir = opendir(newestDirName);
+	chdir(newestDirName);
+	FILE * file;
+	char * name;
+	char eachConnect[256];
+	char type[256];
+	int i = 0; // increment for name
+	int j = 0;
+	while((filename = readdir(gameDir)) != NULL) {
+		if((strcmp(".", filename->d_name) != 0) && (strcmp("..", filename->d_name) != 0)) {
+			file = fopen(filename->d_name, "r");
+			// if ( (file = fopen(filename->d_name, "r")  ) == NULL) perror("Failed to open file:");
+			while(fscanf(file, "ROOM NAME: %ms\n", &name)) {
+				rooms[i].name = name;
+			}
+			while(fscanf(file, "CONNECTION %*d: %s\n", eachConnect)) {
+				rooms[i].numConnection++;
+				// rooms[i].connections[j] = eachConnect;// array of strings
+				strcpy(rooms[i].connections[j], eachConnect);
+				// printf("Connection: %s\n", rooms[i].connections[j]);
+				j++;
+			}
+			while(fscanf(file, "ROOM TYPE: %s", type)) {
+				rooms[i].roomType = type;
+			}
+			i++;
+			fclose(file);
+		}
+	}
+	chdir("..");
+	closedir(gameDir);
 }
 
-// Returns true if a connection can be added from Room x (< 6 outbound connections), false otherwise
-bool CanAddConnectionFrom(Room x) 
-{
-  ...
-}
-// Returns true if a connection from Room x to Room y already exists, false otherwise
-bool ConnectionAlreadyExists(x, y)
-{
-  ...
-}
-
-// Connects Rooms x and y together, does not check if this connection is valid
-void ConnectRoom(Room x, Room y) 
-{
-  ...
+void printStruct(struct room * rooms) {
+	int i, j;
+	for(i = 0; i < 7; i++){
+		printf("Name: %s\n", rooms[i].name);
+		// printf("ID: %d\n", rooms[i].id);
+		// printf("Room Type: %s\n", rooms[i].roomType);
+		printf("Number of Connections: %d\n", rooms[i].numConnection);
+		for(j = 0; j < rooms[i].numConnection; j++) {			
+			printf("CONNECTION %d: %s\n", j+1, rooms[i].connections[j]);
+		}
+	}
 }
 
-// Returns true if Rooms x and y are the same Room, false otherwise
-bool IsSameRoom(Room x, Room y) 
-{
-  ...
-}
+
