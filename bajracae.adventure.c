c0fcaa18853dfcd2c6a0fa4897013909a4d84db2
Aeijan Bajracharya <bajracae@os1.engr.oregonstate.edu>
2019-11-01 20:29:47 -0700

    It's the best I can do

--- bajracae.adventure.c
+++ bajracae.adventure.c
@@ -1,3 +1,10 @@
+///////////////////////////////////////////////////////////////////
+// Title: Program 2 - Adventure
+// Description: A program that runs the adventure game.
+// Author: Aeijan Bajracharya
+// Date: 1 November 2019
+///////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,64 +17,74 @@
 #include <time.h>
 
 // GLOBAL VARIABLES
-pthread_mutex_t mp;
+pthread_mutex_t mp = PTHREAD_MUTEX_INITIALIZER;
 pthread_t mp2;
 
-void findLatestFile(char * newestDirName);
-struct room * createStruct();
-void readRoomNames(char * newestDirName, struct room * rooms);
-void readStructID(struct room * rooms);
-void readRoomType(char * newestDirName, struct room * rooms);
-void fillStruct(char * newestDirName, struct room * rooms);
-void printStruct(struct room * rooms);
+// FUNCTION PROTOTYPES
+void findFile(char * dirName);
+struct room * createStructArr();
+void fillStructArr(char * dirName, struct room * rooms);
+void printStructArr(struct room * rooms);
+bool isConnected(char * input, struct room eachRoom);
+bool isTime(char * input);
 void gameFunction(struct room * rooms);
-char * getValidRoom(struct room eachRoom);
-bool connectionExists(char * userInput, struct room eachRoom);
-bool askTime(char * userInput);
-void * timeIntoTxt(void);
+void * writeCurrentTime(void * args);
 
 struct room {
 	int id;
 	char * name;
-	char * roomType;
+	char * type;
 	int numConnection;
-	// char * connections[7];
-	char connections[7][256];
-	
+	char connectionList[7][256];
 };
 
 int main() {
-
-    char newestDirName[256];
-    findLatestFile(newestDirName); // Name of the latest file
-    struct room * rooms_struct_array = createStruct();
-	fillStruct(newestDirName, rooms_struct_array);
-	// CITE=======================================================================
-	// https://docs.oracle.com/cd/E19455-01/806-5257/sync-112/index.html
+    char dirName[256]; // Stores the name of the most recently modified file
+    findFile(dirName); // Sets dirName to the name of the most recently modified file
+    struct room * structArr = createStructArr(); // New struct array is created and stored into the variable
+	fillStructArr(dirName, structArr); // Reads information from files and stores them into the struct array
+	
+	///////////////////////////////////////////////////////////////////
+	// Source: Piazza
+	// Post #: @261 -> "Spawning a thread only when "time" is called, but not spawning a thread in main"
+	// Author: Arash Shahbaz
+	// Date: 2019
+	// Code version: N/A
+	// Availability: https://piazza.com/class/k0n1xcux3ah4r?cid=261
+	///////////////////////////////////////////////////////////////////
 	// init and lock mutex
+	pthread_mutex_lock(&mp);
 	// spawn time_thread with time function (lock mutex and unlock in time function)
+	pthread_create(&mp2, NULL, writeCurrentTime, 0); 
 	// call gameFunction
+	gameFunction(structArr);
 	// unlock and destroy mutex
-
-	// mp = PTHREAD_MUTEX_INITIALIZER;
-	// pthread_mutex_lock(&mp);
-	// gameFunction(rooms_struct_array);
-	// pthread_mutex_unlock(&mp);
-	// pthread_mutex_destroy(&mp);
-
-	timeIntoTxt();
-	// printStruct(rooms_struct_array);
+	pthread_mutex_unlock(&mp);
+	pthread_mutex_destroy(&mp);
+	///////////////////////////////////////////////////////////////////
+	
     return 0;
 }
 
-void findLatestFile(char * newestDirName) {
-    // -------------------------------------------------------------------------------------------
-    // CITE this
-    // -------------------------------------------------------------------------------------------
+///////////////////////////////////////////////////////////////////
+// Function: void findFile()
+// Description: Finds the last modified directory with "bajracae.rooms"
+//				on its name
+// Parameters: char * dirName
+// Pre-Conditions: Takes in the string directory
+// Post-Conditions: Sets dirName to the name of the last modified directory
+///////////////////////////////////////////////////////////////////
+void findFile(char * dirName) {
+	///////////////////////////////////////////////////////////////////
+	// Source: 2.4 Manipulating Directories
+	// Author: Benjamin Brewster
+	// Date: 2017
+	// Code version: N/A
+	// Availability: https://oregonstate.instructure.com/courses/1738955/pages/2-dot-4-manipulating-directories
+	///////////////////////////////////////////////////////////////////
     int newestDirTime = -1; // Modified timestamp of newest subdir examined
     char targetDirPrefix[32] = "bajracae.rooms."; // Prefix we're looking for
-    //char newestDirName[256]; // Holds the name of the newest dir that contains prefix
-    memset(newestDirName, '\0', sizeof(newestDirName));
+    memset(dirName, '\0', sizeof(dirName));
 
     DIR* dirToCheck; // Holds the directory we're starting in
     struct dirent *fileInDir; // Holds the current subdir of the starting dir
@@ -81,40 +98,53 @@ void findLatestFile(char * newestDirName) {
       {
         if (strstr(fileInDir->d_name, targetDirPrefix) != NULL) // If entry has prefix
         {
-          // printf("Found the prefex: %s\n", fileInDir->d_name);
           stat(fileInDir->d_name, &dirAttributes); // Get attributes of the entry
 
           if ((int)dirAttributes.st_mtime > newestDirTime) // If this time is bigger
           {
             newestDirTime = (int)dirAttributes.st_mtime;
-            memset(newestDirName, '\0', sizeof(newestDirName));
-            strcpy(newestDirName, fileInDir->d_name);
-            // printf("Newer subdir: %s, new time: %d\n", fileInDir->d_name, newestDirTime);
+            memset(dirName, '\0', sizeof(dirName));
+            strcpy(dirName, fileInDir->d_name);
           }
         }
       }
     }
-
     closedir(dirToCheck); // Close the directory we opened
+	///////////////////////////////////////////////////////////////////
 }
 
-struct room * createStruct() {
+///////////////////////////////////////////////////////////////////
+// Function: struct room * createStructArr()
+// Description: Creates a struct room array that holds the  
+// 				name, type, and array of string names of the connectionList
+// Parameters: N/A
+// Pre-Conditions: N/A
+// Post-Conditions: Creates a struct room array
+///////////////////////////////////////////////////////////////////
+struct room * createStructArr() {
 	struct room * rooms = (struct room *)malloc(sizeof(struct room) * 7);
 	int i,j ;
 	for(i = 0; i < 7; i++) {
 		rooms[i].numConnection = 0;
 		for(j = 0; j < 7; j++) {
-			// rooms[i].connections[j]= "";
-			memset(rooms[i].connections[j], 0, 256);
+			memset(rooms[i].connectionList[j], 0, 256);
 		}
 	}
 	return rooms;
 }
 
-void fillStruct(char * newestDirName, struct room * rooms) {
+///////////////////////////////////////////////////////////////////
+// Function: void fillStructArr()
+// Description: Reads information from each of the files and fills
+//				the struct array
+// Parameters: char * dirName, struct room * rooms
+// Pre-Conditions: Takes in the name of the last modified directory and the struct array
+// Post-Conditions: Struct array is filled with read-in information
+///////////////////////////////////////////////////////////////////
+void fillStructArr(char * dirName, struct room * rooms) {
 	struct dirent * filename;
-	DIR * gameDir = opendir(newestDirName);
-	chdir(newestDirName);
+	DIR * gameDir = opendir(dirName);
+	chdir(dirName);
 	FILE * file;
 	char * name;
 	size_t temp;
@@ -131,12 +161,12 @@ void fillStruct(char * newestDirName, struct room * rooms) {
 			}
 			while(fscanf(file, "CONNECTION %*d: %s\n", eachConnect)) {
 				rooms[i].numConnection++;
-				strcpy(rooms[i].connections[j], eachConnect);
+				strcpy(rooms[i].connectionList[j], eachConnect);
 				j++;
 			}
 			getline(&temp2, &temp, file);
 			if(sscanf(temp2, "ROOM TYPE: %ms\n", &type) > 0) {
-				rooms[i].roomType = type;
+				rooms[i].type = type;
 			}
 			i++;
 			j=0;
@@ -147,74 +177,141 @@ void fillStruct(char * newestDirName, struct room * rooms) {
 	closedir(gameDir);
 }
 
-void printStruct(struct room * rooms) {
+///////////////////////////////////////////////////////////////////
+// Function: void printStructArr()
+// Description: Prints the content of all of the variables in the
+//				struct array
+// Parameters: struct room * rooms
+// Pre-Conditions: Takes in the struct array
+// Post-Conditions: Prints the information about each room to the
+//				terminal
+///////////////////////////////////////////////////////////////////
+void printStructArr(struct room * rooms) {
 	int i, j;
 	for(i = 0; i < 7; i++){
 		printf("Name: %s\n", rooms[i].name);
 		printf("Number of Connections: %d\n", rooms[i].numConnection);
 		for(j = 0; j < rooms[i].numConnection; j++) {			
-			printf("CONNECTION %d: %s\n", j+1, rooms[i].connections[j]);
+			printf("CONNECTION %d: %s\n", j+1, rooms[i].connectionList[j]);
 		}
-		printf("Room Type: %s\n", rooms[i].roomType);
+		printf("Room Type: %s\n", rooms[i].type);
 	}
 }
 
+///////////////////////////////////////////////////////////////////
+// Function: isConnected()
+// Description: Checks if the input matches any of the names in the struct's connectionList
+// Parameters: char * input, struct room eachRoom
+// Pre-Conditions: Takes in the user input and the current room's struct
+// Post-Conditions: Returns true if they are the same, else return false
+///////////////////////////////////////////////////////////////////
+bool isConnected(char * input, struct room eachRoom) {
+	int i;
+	for(i = 0; i < eachRoom.numConnection; i++) {
+		if(strcmp(input, eachRoom.connectionList[i]) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+///////////////////////////////////////////////////////////////////
+// Function: isTime()
+// Description: Checks if user input is equal to "time"
+// Parameters: char * input
+// Pre-Conditions: Takes in the user input
+// Post-Conditions: Returns true if the user input is "time", else false
+///////////////////////////////////////////////////////////////////
+bool isTime(char * input) {
+	if(strcmp(input, "time") == 0) {
+		return true;
+	}
+	return false;
+}
+
+///////////////////////////////////////////////////////////////////
+// Function: void gameFunction()
+// Description: Includes the while loop and functions that run the gam
+// Parameters: struct room * rooms
+// Pre-Conditions: Takes in the struct array
+// Post-Conditions: Allows for the game to be playable, game ends when
+//				the user reaches the room with the room type "END_ROOM"
+///////////////////////////////////////////////////////////////////
 void gameFunction(struct room * rooms) {
-	char * currentType = NULL;
+	FILE * file;
+	char * currentRoomType = NULL;
+	char * currentTime = NULL;
+	char * input = NULL;
+	size_t len = 0;
+	char * pathList[256];
+	int countPath = 0;
 	
-	// Find the index of the start room
-	int start = 0;
 	int index = 0;
+	int start;
 	for(start = 0; start < 7; start++) {
-		if(strcmp(rooms[start].roomType, "START_ROOM") == 0) {
+		if(strcmp(rooms[start].type, "START_ROOM") == 0) {
 			index = start;
 		}
 	}
-	
-	char * paths[256];
-	char * userInput = NULL;
-	size_t len = 0;
-	int pathCounter = 0;
 	 
 	int i, j, k;
-	while(currentType != "END_ROOM") {
+	while(currentRoomType != "END_ROOM") {
 		printf("CURRENT LOCATION: %s\n", rooms[index].name);
 		printf("POSSIBLE CONNECTIONS: ");
 		for(j = 0; j < rooms[index].numConnection; j++) {
 			if(j != rooms[index].numConnection - 1) {
-				printf("%s, ", rooms[index].connections[j]);
+				printf("%s, ", rooms[index].connectionList[j]);
 			}
 			else {
-				printf("%s.", rooms[index].connections[j]);
+				printf("%s.", rooms[index].connectionList[j]);
 			}
 		} 
 		printf("\n");
 		printf("WHERE TO? >");
-		getline(&userInput, &len, stdin);
-		userInput[strlen(userInput) - 1] = 0;
-		if((connectionExists(userInput, rooms[index]) != true) && (askTime(userInput) != true)) {
+		getline(&input, &len, stdin);
+		input[strlen(input) - 1] = 0;
+		if((isConnected(input, rooms[index]) != true) && (isTime(input) != true)) {
 			printf("HUH? I DON’T UNDERSTAND THAT ROOM. TRY AGAIN.\n");
 		}
 		else {
-			if(askTime(userInput) == true) {
+			if(isTime(input) == true) {
+				
+				// unlock mutex;
 				pthread_mutex_unlock(&mp);
 				
+				// wait for time thread to execute
+				pthread_join(mp2, NULL);
+				
+				// read from the currentTime
+				printf("\n");
+				file = fopen("currentTime.txt", "r");
+				while(fscanf(file, "%ms", &currentTime) > 0) {
+					printf("%s ", currentTime);
+				}
+				printf("\n");
+				printf("\n");
+				fclose(file);
+				
+				// lock mutex
 				pthread_mutex_lock(&mp);
+				
+				// spawn another time thread
+				pthread_create(&mp2, NULL, writeCurrentTime, 0); 
 			}
 			else {
 				for(i = 0; i < 7; i++) {
-					if(strcmp(userInput, rooms[i].name) == 0) {
+					if(strcmp(input, rooms[i].name) == 0) {
 						index = i;
-						paths[pathCounter] = rooms[index].name;
-						pathCounter++;
+						pathList[countPath] = rooms[index].name;
+						countPath++;
 					}
 				}
-				currentType = rooms[index].roomType;
-				if(strcmp(currentType, "END_ROOM") == 0) {
+				currentRoomType = rooms[index].type;
+				if(strcmp(currentRoomType, "END_ROOM") == 0) {
 					printf("YOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\n");
-					printf("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", pathCounter);
-					for(k = 0; k < pathCounter; k++) {
-						printf("%s\n", paths[k]);
+					printf("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", countPath);
+					for(k = 0; k < countPath; k++) {
+						printf("%s\n", pathList[k]);
 					}
 					break;
 				}
@@ -223,43 +320,40 @@ void gameFunction(struct room * rooms) {
 	}
 }
 
-
-
-// CITE: https://stackoverflow.com/questions/5141960/get-the-current-time-in-c
-void * timeIntoTxt(void) {
-	time_t timer = time(NULL);
-	char s[100];
+///////////////////////////////////////////////////////////////////
+// Function: writeCurrentTime()
+// Description: Writes the current date and time into a file called "currentTime.txt"
+// Parameters: void * args
+// Pre-Conditions: Takes in a void pointer
+// Post-Conditions: Writes the current time into a file and returns null
+///////////////////////////////////////////////////////////////////
+void * writeCurrentTime(void * args) {
+	///////////////////////////////////////////////////////////////////
+	// Source: StackOverFlow -> Get the current time in C
+	// Author: mingos
+	// Date: 2/28/2019
+	// Code version: N/A
+	// Availability: https://stackoverflow.com/questions/5141960/get-the-current-time-in-c
+	///////////////////////////////////////////////////////////////////
+	
+	// unlock the mutex in that function and join it
+	pthread_mutex_lock(&mp);
+	pthread_join(mp2, NULL);
+	
+	time_t timeVar = time(NULL);
+	char timeStr[100];
 	size_t max = 100;
 	struct tm * info;
-
-	info = localtime(&timer);
-	strftime(s, max, "%l:%M%P, %A, %B %d, %Y", info);
+	info = localtime(&timeVar);
+	strftime(timeStr, max, "%l:%M%P, %A, %B %d, %Y", info);
 	
 	FILE * file;
-	printf("Hello\n");
-	fflush(stdout);
 	file = fopen("currentTime.txt", "w+");
-	fprintf(file, s);
+	fprintf(file, timeStr);
 	fclose(file);
+	pthread_mutex_unlock(&mp);
+	return NULL;
 }
 
-// void * 
 
-bool connectionExists(char * userInput, struct room eachRoom) {
-	int i;
-	for(i = 0; i < eachRoom.numConnection; i++) {
-		// printf("Connection %d: %d\n", i, strcmp(userInput, eachRoom.connections[i]));
-		if(strcmp(userInput, eachRoom.connections[i]) == 0) {
-			return true;
-		}
-	}
-	return false;
-}
-
-bool askTime(char * userInput) {
-	if(strcmp(userInput, "time") == 0) {
-		return true;
-	}
-	return false;
-}
 
